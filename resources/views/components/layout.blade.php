45acb2b5729db467f5525f19e3ecba669f309abe
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-11-23 14:23:05 +0300

    Get Your Build Process in Order

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/js/app.js'])
+
 </head>
 
 <body class="h-full">
